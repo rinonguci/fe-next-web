c54f8fdf7db9fa85c0ddff9d57d6689066c504f9
phatphamm01 <84543546+phatphamm01@users.noreply.github.com>
2021-11-13 10:39:13 +0700

    remove bug =.=

--- src/errredux1/actions/categories/index.ts
+++
@@ -1,6 +0,0 @@
-import { ICategory } from "@interfaces/redux";
-import { createAction } from "@reduxjs/toolkit";
-
-export const getCategories = createAction("GET_CATEGORIES");
-export const getCategoriesSuccess = createAction<any>("GET_CATEGORIES_SUCCESS");
-export const getCategoriesError = createAction<string>("GET_CATEGORIES_ERROR");
--- src/errredux1/slides/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-
-import categorisReducer from "./categories";
-
-const rootReducer = combineReducers({ categories: categorisReducer });
-
-export default rootReducer;
-export type RootState = ReturnType<typeof rootReducer>;
